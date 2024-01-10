295a8783714dc00f38cbe2117c1a183e2f4172d7
CutieDeng <CutieDeng@iCloud.com>
2024-01-10 19:35:36 +0800

    Add windows support.
    - Fix the bug for visual studio c++ compile.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, Extension
+import os 
+
+name = os.name 
+if name == 'nt': 
+      args = ['/std:c++20']
+else: 
+      args = ['-std=c++20'] 
 
 module0 = Extension('linked_list',
       sources = ['linked_list/pythonbind.cc'], 
       include_dirs = ['linked_list'],
-      extra_compile_args=["-std=c++20"])  
+      extra_compile_args=args
+) 
 
 setup(
       name = 'linklist',
-      version = '1.6',
+      version = '1.7',
       description = 'Python package with a fast linked_list support. ',
       ext_modules = [module0],
       author = 'Cutie Deng',
